ce8292f1d6f98578911785a7c0a1e7094498274d
Paul Tan <pyokagan@gmail.com>
2016-08-19 21:12:06 +0800

    Inline executeCommand()

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -236,7 +236,37 @@ public class AddressBook {
             }
 
             showToUser("[Command entered:" + userCommand + "]");
-            String feedback = executeCommand(userCommand);
+
+            final String[] commandTypeAndParams = splitCommandWordAndArgs(userCommand);
+            final String commandType = commandTypeAndParams[0];
+            final String commandArgs = commandTypeAndParams[1];
+            String feedback = null;
+            switch (commandType) {
+            case COMMAND_ADD_WORD:
+                feedback = executeAddPerson(commandArgs);
+                break;
+            case COMMAND_FIND_WORD:
+                feedback = executeFindPersons(commandArgs);
+                break;
+            case COMMAND_LIST_WORD:
+                feedback = executeListAllPersonsInAddressBook();
+                break;
+            case COMMAND_DELETE_WORD:
+                feedback = executeDeletePerson(commandArgs);
+                break;
+            case COMMAND_CLEAR_WORD:
+                feedback = executeClearAddressBook();
+                break;
+            case COMMAND_HELP_WORD:
+                feedback = getUsageInfoForAllCommands();
+                break;
+            case COMMAND_EXIT_WORD:
+                executeExitProgramRequest();
+                break;
+            default:
+                feedback = getMessageForInvalidCommandInput(commandType, getUsageInfoForAllCommands());
+                break;
+            }
             showToUser(feedback, DIVIDER);
         }
     }
@@ -264,36 +294,6 @@ public class AddressBook {
      *           COMMAND LOGIC
      * ===========================================
      */
-
-    /**
-     * Executes the command as specified by the {@code userInputString}
-     *
-     * @param userInputString  raw input from user
-     * @return  feedback about how the command was executed
-     */
-    public static String executeCommand(String userInputString) {
-        final String[] commandTypeAndParams = splitCommandWordAndArgs(userInputString);
-        final String commandType = commandTypeAndParams[0];
-        final String commandArgs = commandTypeAndParams[1];
-        switch (commandType) {
-        case COMMAND_ADD_WORD:
-            return executeAddPerson(commandArgs);
-        case COMMAND_FIND_WORD:
-            return executeFindPersons(commandArgs);
-        case COMMAND_LIST_WORD:
-            return executeListAllPersonsInAddressBook();
-        case COMMAND_DELETE_WORD:
-            return executeDeletePerson(commandArgs);
-        case COMMAND_CLEAR_WORD:
-            return executeClearAddressBook();
-        case COMMAND_HELP_WORD:
-            return getUsageInfoForAllCommands();
-        case COMMAND_EXIT_WORD:
-            executeExitProgramRequest();
-        default:
-            return getMessageForInvalidCommandInput(commandType, getUsageInfoForAllCommands());
-        }
-    }
 
     /**
      * Splits raw user input into command word and command arguments string
