5512e7c6e5671903c79be3a80e49cac3e5debd35
Jia Yee <jiayeerawr@gmail.com>
2016-08-21 00:28:03 +0800

    [T2A2] [T2] Lim Jia Yee - Inline method
    The executeExitProgramRequest method calls the exitProgram method without
    value-adding (no new parameters or logic). As such, replacing all calls to executeExitProgramRequest with exitProgram is safe.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -362,7 +362,7 @@ public class AddressBook {
         case COMMAND_HELP_WORD:
             return getUsageInfoForAllCommands();
         case COMMAND_EXIT_WORD:
-            executeExitProgramRequest();
+        	exitProgram();
         default:
             return getMessageForInvalidCommandInput(commandType, getUsageInfoForAllCommands());
         }
@@ -557,15 +557,6 @@ public class AddressBook {
         ArrayList<String[]> toBeDisplayed = getAllPersonsInAddressBook();
         showToUser(toBeDisplayed);
         return getMessageForPersonsDisplayedSummary(toBeDisplayed);
-    }
-
-    /**
-     * Request to terminate the program.
-     *
-     * @return feedback display message for the operation result
-     */
-    private static void executeExitProgramRequest() {
-        exitProgram();
     }
 
     /*
