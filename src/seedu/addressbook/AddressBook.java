a0bf6ad7f4ee20f6a3276bb71456f19ef89effae
Wang Leng Tan <tanwangleng@outlook.com>
2016-08-21 16:35:40 +0800

    Extract method executeCommandsUntilExit()

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -206,7 +206,11 @@ public class AddressBook {
         showWelcomeMessage();
         processProgramArgs(args);
         loadDataFromStorage();
-        while (true) {
+        executeCommandsUntilExit();
+    }
+
+    private static void executeCommandsUntilExit() {
+	while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
