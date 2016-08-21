406da37e639c0c2fc5c2ec4af4b43f89d83ad720
cadmusthefounder <cadmusthefounder@gmail.com>
2016-08-21 21:26:00 +0800

    Revert "reduce SLAP for exercise 1"
    This reverts commit 1ee7f9edeade2216292b78851a8c953ebc19b73b.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -198,19 +198,7 @@ public class AddressBook {
      */
     public static void main(String[] args) {
         showWelcomeMessage();
-//      processProgramArgs(args);
-        if (args.length >= 2) {
-            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
-            exitProgram();
-        }
-
-        if (args.length == 1) {
-            setupGivenFileForStorage(args[0]);
-        }
-
-        if(args.length == 0) {
-            setupDefaultFileForStorage();
-        }
+        processProgramArgs(args);
         loadDataFromStorage();
         while (true) {
             String userCommand = getUserInput();
@@ -264,7 +252,7 @@ public class AddressBook {
      *
      * @param args full program arguments passed to application main method
      */
-/*    private static void processProgramArgs(String[] args) {
+    private static void processProgramArgs(String[] args) {
         if (args.length >= 2) {
             showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
             exitProgram();
@@ -277,7 +265,7 @@ public class AddressBook {
         if(args.length == 0) {
             setupDefaultFileForStorage();
         }
-    }*/
+    }
 
     /**
      * Sets up the storage file based on the supplied file path.
