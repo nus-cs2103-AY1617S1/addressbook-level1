1a511410f1a4b75500c997171ff417e51af97f90
Pu Xiao <xp.xiaopu@gmail.com>
2016-08-21 22:13:19 +0800

    Replace magic number `3` as PERSON_DATA_COUNT

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -126,6 +126,11 @@ public class AddressBook {
     private enum PersonProperty {
     		NAME, EMAIL, PHONE;
     };
+    
+    /**
+     * The number of data elements for a single person.
+     */
+    private static final int PERSON_DATA_COUNT = 3;
 
 
     /**
@@ -907,7 +912,7 @@ public class AddressBook {
     private static boolean isPersonDataExtractableFrom(String personData) {
         final String matchAnyPersonDataPrefix = PERSON_DATA_PREFIX_PHONE + '|' + PERSON_DATA_PREFIX_EMAIL;
         final String[] splitArgs = personData.trim().split(matchAnyPersonDataPrefix);
-        return splitArgs.length == 3 // 3 arguments
+        return splitArgs.length == PERSON_DATA_COUNT
                 && !splitArgs[0].isEmpty() // non-empty arguments
                 && !splitArgs[1].isEmpty()
                 && !splitArgs[2].isEmpty();
