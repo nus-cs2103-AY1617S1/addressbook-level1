08724e105f94f8bf15fdabd6497d9041927afb55
qiwei24 <e0002931@u.nus.edu>
2016-08-22 21:45:47 +0800

    Extract method getAndShowInput(); Editted if statement;

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -41,6 +41,7 @@ public class AddressBook {
      */
     private static final String VERSION = "AddessBook Level 1 - Version 1.0";
 
+  
     /**
      * A decorative prefix added to the beginning of lines printed by AddressBook
      */
@@ -202,13 +203,17 @@ public class AddressBook {
         showWelcomeMessage();
         processProgramArgs(args);
         loadDataFromStorage();
-        while (true) {
+        getAndShowInput();
+    }
+
+	private static void getAndShowInput() {
+		while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
         }
-    }
+	}
 
     /*
      * ==============NOTE TO STUDENTS======================================
@@ -260,11 +265,11 @@ public class AddressBook {
             exitProgram();
         }
 
-        if (args.length == 1) {
+        else if (args.length == 1) {
             setupGivenFileForStorage(args[0]);
         }
 
-        if(args.length == 0) {
+        else if(args.length == 0) {
             setupDefaultFileForStorage();
         }
     }
