73830b43515f001b8cf0bc59fdcd01301acd4b37
leeyimin <imleeyimin@gmail.com>
2016-08-24 03:14:00 +0800

    Inline processProgramArgs

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -184,7 +184,16 @@ public class AddressBook {
      */
     public static void main(String[] args) {
         showWelcomeMessage();
-        processProgramArgs(args);
+        
+        if (args.length >= 2) {
+            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+            exitProgram();
+        }
+        if (args.length == 1) {
+            setupGivenFileForStorage(args[0]);
+        }
+        setupDefaultFileForStorage();
+        
         loadDataFromStorage();
         while (true) {
             String userCommand = getUserInput();
