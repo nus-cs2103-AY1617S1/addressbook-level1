ed512e499f60c16157823e90b979f19e53a28cf0
“marcusngwj” <marcusngwj@msn.com>
2016-08-23 22:23:08 +0800

    Added accidental deletion of line

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -208,7 +208,8 @@ public class AddressBook {
 						"===================================================");
 				System.exit(0);
 			}
-
+			
+			storageFilePath = args[0];
 			final File storageFile = new File(args[0]);
 			if (storageFile.exists()) {
 				return;
