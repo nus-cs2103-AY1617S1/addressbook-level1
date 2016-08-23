14303e0a5342b21dc52425270df185e6a320a55a
Brandon <brandonyeoxg@gmail.com>
2016-08-23 18:39:35 +0800

    Added find functionality to be not case sensitive

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -443,7 +443,9 @@ public class AddressBook {
      * @return set of keywords as specified by args
      */
     private static Set<String> extractKeywordsFromFindPersonArgs(String findPersonCommandArgs) {
-        return new HashSet<>(splitByWhitespace(findPersonCommandArgs.trim()));
+        String trimmedArgs = findPersonCommandArgs.trim();
+        String lowerCaseArgs = trimmedArgs.toLowerCase();
+		return new HashSet<>(splitByWhitespace(lowerCaseArgs));
     }
 
     /**
@@ -455,7 +457,9 @@ public class AddressBook {
     private static ArrayList<String[]> getPersonsWithNameContainingAnyKeyword(Collection<String> keywords) {
         final ArrayList<String[]> matchedPersons = new ArrayList<>();
         for (String[] person : getAllPersonsInAddressBook()) {
-            final Set<String> wordsInName = new HashSet<>(splitByWhitespace(getNameFromPerson(person)));
+            String nameFromPerson = getNameFromPerson(person);
+			ArrayList<String> splitByWhitespace = splitByWhitespace(nameFromPerson.toLowerCase());
+			final Set<String> wordsInName = new HashSet<>(splitByWhitespace);
             if (!Collections.disjoint(wordsInName, keywords)) {
                 matchedPersons.add(person);
             }
