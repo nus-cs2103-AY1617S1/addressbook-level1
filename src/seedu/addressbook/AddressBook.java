5e3ca23bb59f4c58fb937aa59ea470cebad4cb0d
unusep <unusep@gmail.com>
2016-08-23 22:30:37 +0800

    Change String array variable name split -> splits

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -363,8 +363,8 @@ public class AddressBook {
      * @return  size 2 array; first element is the command type and second element is the arguments string
      */
     private static String[] splitCommandWordAndArgs(String rawUserInput) {
-        final String[] split =  rawUserInput.trim().split("\\s+", 2);
-        return split.length == 2 ? split : new String[] { split[0] , "" }; // else case: no parameters
+        final String[] splits =  rawUserInput.trim().split("\\s+", 2);
+        return splits.length == 2 ? splits : new String[] { splits[0] , "" }; // else case: no parameters
     }
 
     /**
