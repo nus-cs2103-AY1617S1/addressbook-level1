65bb06206acb3ac1e63c1a66e710151b65acb4ce
clarencechee <clarencechee94@gmail.com>
2016-08-24 18:59:12 +0800

    Rename boolean variable according to guidelines

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -890,11 +890,11 @@ public class AddressBook
      */
     private static boolean deletePersonFromAddressBook(String[] exactPerson)
     {
-        final boolean changed = ALL_PERSONS.remove(exactPerson);
-        if (changed) {
+        final boolean isChanged = ALL_PERSONS.remove(exactPerson);
+        if (isChanged) {
             savePersonsToFile(getAllPersonsInAddressBook(), storageFilePath);
         }
-        return changed;
+        return isChanged;
     }
     
     /**
