30cf0524ec661d8f6ee1c67eda2940c8749ad9a6
takukou <jouq1110@hotmail.com>
2016-08-24 21:47:33 +0800

    Inline method makePersonFromData(), Inline method removePrefixSign(), Inline method isPersonNameValid(), Inline method isPersonPhoneValid(), Inline method isPersonEmailValid(), Inline method isPersonDataValid()

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -676,22 +676,6 @@ public class AddressBook {
      * ===========================================
      */
 
-    /**
-     * Create a person for use in the internal data.
-     *
-     * @param name of person
-     * @param phone without data prefix
-     * @param email without data prefix
-     * @return constructed person
-     */
-    private static String[] makePersonFromData(String name, String phone, String email) {
-        final String[] person = new String[PERSON_DATA_COUNT];
-        person[PERSON_DATA_INDEX_NAME] = name;
-        person[PERSON_DATA_INDEX_PHONE] = phone;
-        person[PERSON_DATA_INDEX_EMAIL] = email;
-        return person;
-    }
-
     /*
      * ==============NOTE TO STUDENTS======================================
      * Note the use of Java's new 'Optional' feature to indicate that
@@ -710,13 +694,15 @@ public class AddressBook {
         if (!isPersonDataExtractableFrom(encoded)) {
             return Optional.empty();
         }
-        final String[] decodedPerson = makePersonFromData(
-                extractNameFromPersonString(encoded),
-                extractPhoneFromPersonString(encoded),
-                extractEmailFromPersonString(encoded)
-        );
+        final String[] person = new String[PERSON_DATA_COUNT];
+        person[PERSON_DATA_INDEX_NAME] = extractNameFromPersonString(encoded);
+        person[PERSON_DATA_INDEX_PHONE] = extractPhoneFromPersonString(encoded);
+        person[PERSON_DATA_INDEX_EMAIL] = extractEmailFromPersonString(encoded);
+        final String[] decodedPerson = person;
         // check that the constructed person is valid
-        return isPersonDataValid(decodedPerson) ? Optional.of(decodedPerson) : Optional.empty();
+        return decodedPerson[PERSON_DATA_INDEX_NAME].matches("(\\w|\\s)+")
+        && decodedPerson[PERSON_DATA_INDEX_PHONE].matches("\\d+")
+        && decodedPerson[PERSON_DATA_INDEX_EMAIL].matches("\\S+@\\S+\\.\\S+") ? Optional.of(decodedPerson) : Optional.empty();
     }
 
     /**
@@ -780,14 +766,11 @@ public class AddressBook {
 
         // phone is last arg, target is from prefix to end of string
         if (indexOfPhonePrefix > indexOfEmailPrefix) {
-            return removePrefixSign(encoded.substring(indexOfPhonePrefix, encoded.length()).trim(),
-                    PERSON_DATA_PREFIX_PHONE);
+            return encoded.substring(indexOfPhonePrefix, encoded.length()).trim().replace(PERSON_DATA_PREFIX_PHONE, "");
 
         // phone is middle arg, target is from own prefix to next prefix
         } else {
-            return removePrefixSign(
-                    encoded.substring(indexOfPhonePrefix, indexOfEmailPrefix).trim(),
-                    PERSON_DATA_PREFIX_PHONE);
+            return encoded.substring(indexOfPhonePrefix, indexOfEmailPrefix).trim().replace(PERSON_DATA_PREFIX_PHONE, "");
         }
     }
 
@@ -803,67 +786,12 @@ public class AddressBook {
 
         // email is last arg, target is from prefix to end of string
         if (indexOfEmailPrefix > indexOfPhonePrefix) {
-            return removePrefixSign(encoded.substring(indexOfEmailPrefix, encoded.length()).trim(),
-                    PERSON_DATA_PREFIX_EMAIL);
+            return encoded.substring(indexOfEmailPrefix, encoded.length()).trim().replace(PERSON_DATA_PREFIX_EMAIL, "");
 
         // email is middle arg, target is from own prefix to next prefix
         } else {
-            return removePrefixSign(
-                    encoded.substring(indexOfEmailPrefix, indexOfPhonePrefix).trim(),
-                    PERSON_DATA_PREFIX_EMAIL);
+            return encoded.substring(indexOfEmailPrefix, indexOfPhonePrefix).trim().replace(PERSON_DATA_PREFIX_EMAIL, "");
         }
-    }
-
-    /**
-     * Validates a person's data fields
-     *
-     * @param person String array representing the person (used in internal data)
-     * @return whether the given person has valid data
-     */
-    private static boolean isPersonDataValid(String[] person) {
-        return isPersonNameValid(person[PERSON_DATA_INDEX_NAME])
-                && isPersonPhoneValid(person[PERSON_DATA_INDEX_PHONE])
-                && isPersonEmailValid(person[PERSON_DATA_INDEX_EMAIL]);
-    }
-
-    /*
-     * ==============NOTE TO STUDENTS======================================
-     * Note the use of 'regular expressions' in the method below.
-     * Regular expressions can be very useful in checking if a a string
-     * follows a sepcific format.
-     * ====================================================================
-     */
-    /**
-     * Validates string as a legal person name
-     *
-     * @param name to be validated
-     * @return whether arg is a valid person name
-     */
-    private static boolean isPersonNameValid(String name) {
-        return name.matches("(\\w|\\s)+");  // name is nonempty mixture of alphabets and whitespace
-        //TODO: implement a more permissive validation
-    }
-
-    /**
-     * Validates string as a legal person phone number
-     *
-     * @param phone to be validated
-     * @return whether arg is a valid person phone number
-     */
-    private static boolean isPersonPhoneValid(String phone) {
-        return phone.matches("\\d+");    // phone nonempty sequence of digits
-        //TODO: implement a more permissive validation
-    }
-
-    /**
-     * Validates string as a legal person email
-     *
-     * @param email to be validated
-     * @return whether arg is a valid person email
-     */
-    private static boolean isPersonEmailValid(String email) {
-        return email.matches("\\S+@\\S+\\.\\S+"); // email is [non-whitespace]@[non-whitespace].[non-whitespace]
-        //TODO: implement a more permissive validation
     }
 
 
@@ -883,17 +811,5 @@ public class AddressBook {
      *         UTILITY METHODS
      * ============================
      */
-
-    /**
-     * Removes sign(p/, d/, etc) from parameter string
-     *
-     * @param s  Parameter as a string
-     * @param sign  Parameter sign to be removed
-     *
-     * @return  Priority string without p/
-     */
-    private static String removePrefixSign(String s, String sign) {
-        return s.replace(sign, "");
-    }
 
 }
