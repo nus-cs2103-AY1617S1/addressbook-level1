0bb97f9b362cbc79e2326c73477ee014d3e8607d
Jasper Lee <jasper.lee@u.nus.edu>
2016-08-24 11:18:44 +0800

    Inline processProgramArgs

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -200,21 +200,10 @@ public class AddressBook {
      */
     public static void main(String[] args) {
         showWelcomeMessage();
-        if (args.length >= 2) {
-		    showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
-		    exitProgram();
-		}
-		
-		if (args.length == 1) {
-		    setupGivenFileForStorage(args[0]);
-		}
-		
-		if(args.length == 0) {
-		    setupDefaultFileForStorage();
-		}
+        processProgramArgs(args);
         loadDataFromStorage();
         while (true) {
-            String userCommand = getUserInput();
+            String userCommand = getUserInput();	
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
