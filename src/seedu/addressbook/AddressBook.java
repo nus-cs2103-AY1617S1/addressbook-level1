f4266bd9f878d4b052b74fbb84af3b20fd3d570a
clarencechee <clarencechee94@gmail.com>
2016-08-24 23:27:46 +0800

    Remove unused import

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Scanner;
