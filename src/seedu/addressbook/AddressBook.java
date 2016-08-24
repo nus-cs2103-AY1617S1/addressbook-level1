a944f4e8f7e7e71d721793622de1eeaa06531548
Vishnu Prem <vishnu@vishnuprem.com>
2016-08-24 09:52:34 +0800

    Rename changed to personIsChanged for clarity

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -783,11 +783,11 @@ public class AddressBook {
      * @return true if the given person was found and deleted in the model
      */
     private static boolean deletePersonFromAddressBook(String[] exactPerson) {
-        final boolean changed = ALL_PERSONS.remove(exactPerson);
-        if (changed) {
+        final boolean personIsChanged = ALL_PERSONS.remove(exactPerson);
+        if (personIsChanged) {
             savePersonsToFile(getAllPersonsInAddressBook(), storageFilePath);
         }
-        return changed;
+        return personIsChanged;
     }
 
     /**
