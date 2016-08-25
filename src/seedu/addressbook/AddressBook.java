f7643731f94341c3895afdfe287a1dbb86c39189
eugenepeh <e0012835@u.nus.edu>
2016-08-25 22:36:34 +0800

    Extract method runProgramUntilExit()

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -204,13 +204,17 @@ public class AddressBook {
         showWelcomeMessage();
         processProgramArgs(args);
         loadDataFromStorage();
-        while (true) {
+        runProgramUntilExit();
+    }
+
+	private static void runProgramUntilExit() {
+		while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
         }
-    }
+	}
 
     /*
      * ==============NOTE TO STUDENTS======================================
