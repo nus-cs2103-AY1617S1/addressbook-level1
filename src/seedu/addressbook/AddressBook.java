dcba4f67842bd7cad8665e4ba2997f0a536c4f8a
Dylan Chew <dylanchew@206-204.priv19.nus.edu.sg>
2016-08-25 15:45:31 +0800

    import with * instead of specific libraries

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -5,19 +5,10 @@ package seedu.addressbook;
  * ====================================================================
  */
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 /* ==============NOTE TO STUDENTS======================================
  * This class header comment below is brief because details of how to
