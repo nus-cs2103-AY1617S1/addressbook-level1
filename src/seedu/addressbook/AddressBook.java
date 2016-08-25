70c8a3a27308e354c28adfd0e4e1c41f47abdda4
patcho <patcho168@gmail.com>
2016-08-25 12:09:32 +0800

    added else if in processProgramArgs

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -260,11 +260,11 @@ public class AddressBook {
             exitProgram();
         }
 
-        if (args.length == 1) {
+        else if (args.length == 1) {
             setupGivenFileForStorage(args[0]);
         }
 
-        if(args.length == 0) {
+        else if(args.length == 0) {
             setupDefaultFileForStorage();
         }
     }
