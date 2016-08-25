091d26519677356b3cfd57ed2f7241f046f5ceaf
JC <knyt.jc@hotmail.com>
2016-08-25 21:39:31 +0800

    Extract method processInputExecuteCommand()

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -202,13 +202,20 @@ public class AddressBook {
         showWelcomeMessage();
         processProgramArgs(args);
         loadDataFromStorage();
-        while (true) {
+        processInputExecuteCommand();
+    }
+    
+    /**
+     * Processes the series of user inputs and execute corresponding commands.
+     */
+	public static void processInputExecuteCommand() {
+		while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
         }
-    }
+	}
 
     /*
      * ==============NOTE TO STUDENTS======================================
