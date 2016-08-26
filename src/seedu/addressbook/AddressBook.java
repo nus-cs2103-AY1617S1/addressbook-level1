af4bdc56cdac6aab10606dc9467daf716f3744f3
Zhe Min <chiazhemin@gmail.com>
2016-08-26 12:52:12 +0800

    Add compartor.util

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Scanner;
