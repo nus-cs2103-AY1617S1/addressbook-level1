b1c9dd6a6f2f69ef10ab37cb54a180574c6c2475
woo hian chua <woo hian chua>
2016-08-26 18:52:08 +0800

    Refactored

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -202,13 +202,17 @@ public class AddressBook {
         showWelcomeMessage();
         processProgramArgs(args);
         loadDataFromStorage();
-        while (true) {
+        readAndExecuteInput();
+    }
+
+	private static void readAndExecuteInput() {
+		while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
         }
-    }
+	}
 
     /*
      * ==============NOTE TO STUDENTS======================================
