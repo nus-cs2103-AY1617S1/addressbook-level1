c6cb26f9e7c51b29a2dcbce77c6ada2be5d277fb
DongZaiEr <lixiaowei0535@gmail.com>
2016-08-26 17:56:10 +0800

    replace processProgramArgs+mess up the code

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -200,15 +200,31 @@ public class AddressBook {
      */
     public static void main(String[] args) {
         showWelcomeMessage();
-        processProgramArgs(args);
+        	if (args.length >= 2) {
+        		showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+        		exitProgram();
+        	}
+        	
+        	if (args.length == 1) {
+        		setupGivenFileForStorage(args[0]);
+        	}
+        	
+        	if(args.length == 0) {
+        		setupDefaultFileForStorage();
+        	}
+        
         loadDataFromStorage();
-        while (true) {
+        run();
+    }
+
+	private static void run() {
+		while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
         }
-    }
+	}
 
     /*
      * ==============NOTE TO STUDENTS======================================
@@ -254,20 +270,6 @@ public class AddressBook {
      *
      * @param args full program arguments passed to application main method
      */
-    private static void processProgramArgs(String[] args) {
-        if (args.length >= 2) {
-            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
-            exitProgram();
-        }
-
-        if (args.length == 1) {
-            setupGivenFileForStorage(args[0]);
-        }
-
-        if(args.length == 0) {
-            setupDefaultFileForStorage();
-        }
-    }
 
     /**
      * Sets up the storage file based on the supplied file path.
