e9e2bb5253d0dc4eaa42bd747b0ffddf438cb46b
Jihui Xie <xjh125470939@gmail.com>
2016-08-26 15:02:28 +0800

    no message

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -258,13 +258,9 @@ public class AddressBook {
         if (args.length >= 2) {
             showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
             exitProgram();
-        }
-
-        if (args.length == 1) {
+        }if (args.length == 1) {
             setupGivenFileForStorage(args[0]);
-        }
-
-        if(args.length == 0) {
+        }if(args.length == 0) {
             setupDefaultFileForStorage();
         }
     }
@@ -639,7 +635,10 @@ public class AddressBook {
      */
     private static String getMessageForFormattedPersonData(String[] person) {
         return String.format(MESSAGE_DISPLAY_PERSON_DATA,
-                getNameFromPerson(person), getPhoneFromPerson(person), getEmailFromPerson(person));
+                getNameFromPerson(person), 
+                getPhoneFromPerson(person), 
+                getEmailFromPerson(person)
+                );
     }
 
     /**
