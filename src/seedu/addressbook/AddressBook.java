e143a7bece353d05ca26699268898e73ec373182
ShengXuan <shengxuan14@gmail.com>
2016-08-28 20:49:35 +0800

    Make the code worse.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -199,14 +199,25 @@ public class AddressBook {
      * ====================================================================
      */
     public static void main(String[] args) {
-        showWelcomeMessage();
-        processProgramArgs(args);
-        loadDataFromStorage();
+    	showToUser(DIVIDER, DIVIDER, VERSION, MESSAGE_WELCOME, DIVIDER);
+        if (args.length >= 2) {
+            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+            exitProgram();
+        }
+
+        if (args.length == 1) {
+            setupGivenFileForStorage(args[0]);
+        }
+
+        if(args.length == 0) {
+            setupDefaultFileForStorage();
+        }
+        initialiseAddressBookModel(loadPersonsFromFile(storageFilePath));
         while (true) {
-            String userCommand = getUserInput();
-            echoUserCommand(userCommand);
-            String feedback = executeCommand(userCommand);
-            showResultToUser(feedback);
+            String i = getUserInput();
+            echoUserCommand(i);
+            String j = executeCommand(i);
+            showResultToUser(j);
         }
     }
 
@@ -254,20 +265,20 @@ public class AddressBook {
      *
      * @param args full program arguments passed to application main method
      */
-    private static void processProgramArgs(String[] args) {
-        if (args.length >= 2) {
-            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
-            exitProgram();
-        }
-
-        if (args.length == 1) {
-            setupGivenFileForStorage(args[0]);
-        }
-
-        if(args.length == 0) {
-            setupDefaultFileForStorage();
-        }
-    }
+//    private static void processProgramArgs(String[] args) {
+//        if (args.length >= 2) {
+//            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+//            exitProgram();
+//        }
+//
+//        if (args.length == 1) {
+//            setupGivenFileForStorage(args[0]);
+//        }
+//
+//        if(args.length == 0) {
+//            setupDefaultFileForStorage();
+//        }
+//    }
 
     /**
      * Sets up the storage file based on the supplied file path.
