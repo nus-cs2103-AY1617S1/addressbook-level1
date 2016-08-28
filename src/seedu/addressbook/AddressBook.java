0c8bf33fd0cd1c00e1a8289c062a6a51c92ba2aa
e0003315 <hujingrui@hotmail.com>
2016-08-28 17:53:56 +0800

    Extract method from main method

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -202,13 +202,17 @@ public class AddressBook {
         showWelcomeMessage();
         processProgramArgs(args);
         loadDataFromStorage();
-        while (true) {
+        showResultToUser();
+    }
+
+	private static void showResultToUser() {
+		while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
         }
-    }
+	}
 
     /*
      * ==============NOTE TO STUDENTS======================================
