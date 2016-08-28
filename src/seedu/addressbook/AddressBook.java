4846597855deb25c086ea8754cab067493e5d9a5
Ying Ying Choi <yingying_choi@hotmail.com>
2016-08-28 13:40:01 +0800

    Added case insensitive search.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -428,7 +428,7 @@ public class AddressBook {
      * @return feedback display message for the operation result
      */
     private static String executeFindPersons(String commandArgs) {
-        final Set<String> keywords = extractKeywordsFromFindPersonArgs(commandArgs);
+        final Set<String> keywords = extractKeywordsFromFindPersonArgs(commandArgs.toLowerCase());
         final ArrayList<String[]> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
         showToUser(personsFound);
         return getMessageForPersonsDisplayedSummary(personsFound);
@@ -769,9 +769,16 @@ public class AddressBook {
      * @param person to add
      */
     private static void addPersonToAddressBook(String[] person) {
+    	makePersonInformationLowerCase(person);
         ALL_PERSONS.add(person);
         savePersonsToFile(getAllPersonsInAddressBook(), storageFilePath);
     }
+
+	private static void makePersonInformationLowerCase(String[] person) {
+		for(String p: person) {
+    		p.toLowerCase();
+    	}
+	}
 
     /**
      * Deletes a person from the address book, target is identified by it's absolute index in the full list.
