fae85bbd08e97133e90438a2d8372f822d861482
Niveetha <niveetha.nair@gmail.com>
2016-08-31 18:02:06 +0800

    showToUser drops abstraction
    showToUser != function, but now a System.out.println()

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -209,7 +209,22 @@ public class AddressBook {
 	 */
 	public static void main(String[] args) {
 		showWelcomeMessage();
-		processProgramArgs(args);	
+		//processProgramArgs(args);
+		
+		if (args.length >= 2) {
+			//showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
+			System.out.println("MESSAGE_INVALID_PROGRAM_ARGS");
+			exitProgram();
+		}
+
+		if (args.length == 1) {
+			setupGivenFileForStorage(args[0]);
+		}
+
+		if (args.length == 0) {
+			setupDefaultFileForStorage();
+		}
+		
 		loadDataFromStorage();
 		while (true) {
 			String userCommand = getUserInput();
@@ -264,7 +279,7 @@ public class AddressBook {
 	 * @param args
 	 *            full program arguments passed to application main method
 	 */
-	private static void processProgramArgs(String[] args) {
+	/*private static void processProgramArgs(String[] args) {
 		if (args.length >= 2) {
 			showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
 			exitProgram();
@@ -277,7 +292,7 @@ public class AddressBook {
 		if (args.length == 0) {
 			setupDefaultFileForStorage();
 		}
-	}
+	}*/
 
 	/**
 	 * Sets up the storage file based on the supplied file path. Creates the
