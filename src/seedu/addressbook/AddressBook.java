79764db4678d0538b87bb9ff610688b8ae2f6759
FAMILYPC\Musa <ghurabah93@hotmail.com>
2016-08-31 15:44:44 +0800

    Revert "Reduce SLAP of method"
    This reverts commit caf12a7ed1f3f590602eb9bb6c9c00b39626d87d.

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -200,28 +200,19 @@ public class AddressBook {
      */
     public static void main(String[] args) {
         showWelcomeMessage();
-        //processProgramArgs(args);
-        if (args.length >= 2) {
-            showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
-            exitProgram();
-        }
-
-        if (args.length == 1) {
-            setupGivenFileForStorage(args[0]);
-        }
-
-        if(args.length == 0) {
-            setupDefaultFileForStorage();
-        }
+        processProgramArgs(args);
         loadDataFromStorage();
-       while (true) {
+        getUserInputContinuously();
+    }
+
+	private static void getUserInputContinuously() {
+		while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
         }
-    }
-
+	}
 
     /*
      * ==============NOTE TO STUDENTS======================================
