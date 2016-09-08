6dc8ac3c9392492d1d86a1008951b073ecd5b7b2
evilmtv <diaryofnotes@gmail.com>
2016-09-08 14:47:02 +0800

    LO-Refactor

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -199,27 +199,20 @@ public class AddressBook {
      * ====================================================================
      */
     public static void main(String[] args) {
-        showToUser(DIVIDER, DIVIDER, VERSION, MESSAGE_WELCOME, DIVIDER);
-        if (args.length >= 2) {
-		    showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
-		    exitProgram();
-		}
-		
-		if (args.length == 1) {
-		    setupGivenFileForStorage(args[0]);
-		}
-		
-		if(args.length == 0) {
-		    setupDefaultFileForStorage();
-		}
-        initialiseAddressBookModel(loadPersonsFromFile(storageFilePath));
+        showWelcomeMessage();
+        processProgramArgs(args);
+        loadDataFromStorage();
         while (true) {
-            String userCommand = getUserInput();
-            echoUserCommand(userCommand);
-            String feedback = executeCommand(userCommand);
-            showResultToUser(feedback);
+            doInput();
         }
     }
+
+	private static void doInput() {
+		String userCommand = getUserInput();
+		echoUserCommand(userCommand);
+		String feedback = executeCommand(userCommand);
+		showResultToUser(feedback);
+	}
 
     /*
      * ==============NOTE TO STUDENTS======================================
