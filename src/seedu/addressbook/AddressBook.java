ae74449895dd36501bec416522c5597bc27c576a
CHQ\Ching <hqching@gmail.com>
2017-01-26 01:25:28 +0800

    change method signature removePrefix to removePersonDataPrefix

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -1006,12 +1006,12 @@ public class AddressBook {
 
         // phone is last arg, target is from prefix to end of string
         if (indexOfPhonePrefix > indexOfEmailPrefix) {
-            return removePrefixSign(encoded.substring(indexOfPhonePrefix, encoded.length()).trim(),
+            return removePersonDataPrefix(encoded.substring(indexOfPhonePrefix, encoded.length()).trim(),
                     PERSON_DATA_PREFIX_PHONE);
 
             // phone is middle arg, target is from own prefix to next prefix
         } else {
-            return removePrefixSign(encoded.substring(indexOfPhonePrefix, indexOfEmailPrefix).trim(),
+            return removePersonDataPrefix(encoded.substring(indexOfPhonePrefix, indexOfEmailPrefix).trim(),
                     PERSON_DATA_PREFIX_PHONE);
         }
     }
@@ -1028,12 +1028,12 @@ public class AddressBook {
 
         // email is last arg, target is from prefix to end of string
         if (indexOfEmailPrefix > indexOfPhonePrefix) {
-            return removePrefixSign(encoded.substring(indexOfEmailPrefix, encoded.length()).trim(),
+            return removePersonDataPrefix(encoded.substring(indexOfEmailPrefix, encoded.length()).trim(),
                     PERSON_DATA_PREFIX_EMAIL);
 
             // email is middle arg, target is from own prefix to next prefix
         } else {
-            return removePrefixSign(encoded.substring(indexOfEmailPrefix, indexOfPhonePrefix).trim(),
+            return removePersonDataPrefix(encoded.substring(indexOfEmailPrefix, indexOfPhonePrefix).trim(),
                     PERSON_DATA_PREFIX_EMAIL);
         }
     }
@@ -1157,8 +1157,8 @@ public class AddressBook {
      * @param sign Parameter sign to be removed
      * @return string without the sign
      */
-    private static String removePrefixSign(String s, String sign) {
-        return s.replace(sign, "");
+    private static String removePersonDataPrefix(String s, String prefix) {
+        return s.replace(prefix, "");
     }
 
     /**
